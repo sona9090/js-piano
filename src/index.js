1378a53c1c92ee26c3f3ef41f5a48e5405a661fd
sona9090 <100478518+sona9090@users.noreply.github.com>
2023-02-28 12:08:56 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+const notesNames = [
+  "c",
+  "cis",
+  "d",
+  "dis",
+  "e",
+  "f",
+  "fis",
+  "g",
+  "gis",
+  "a",
+  "b",
+  "h"
+];
+
+const keysSelector = ".keyboard > div";
+document.querySelectorAll(keysSelector).forEach((pianoKey, index) => {
+  let noteOrder = index;
+  let octaveNum = 4 + Math.floor(noteOrder / notesNames.length);
+
+  while (noteOrder > notesNames.length - 1) {
+    noteOrder = noteOrder - notesNames.length;
+  }
+
+  let sound = `./src/notes/${notesNames[noteOrder]}${octaveNum}.mp3`;
+
+  pianoKey.innerHTML = `<audio controls>
+    <source src=${sound} type="audio/mpeg" />
+  </audio>`;
+
+  const music = pianoKey.querySelector("audio");
+
+  pianoKey.addEventListener("mousedown", () => music.play());
+  pianoKey.addEventListener("mouseup", () => music.pause());
+});
